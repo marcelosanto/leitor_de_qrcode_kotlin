27175ea415757a4eaf5d117518a68f7c3a13b8c2
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-13 11:55:10 -0300

    ajustes na tela e binding dos elementos

--- app/src/main/java/com/marcelo/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/marcelo/qrcodescanner/MainActivity.kt
@@ -2,10 +2,48 @@ package com.marcelo.qrcodescanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import com.marcelo.qrcodescanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var cardView1:CardView? = null
+    var cardView2:CardView? = null
+    var btnScan:Button? = null
+    var btnEntercode:Button? = null
+    var editCode:EditText? = null
+    var tvText:TextView? = null
+
+    private  lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        cardView1 = binding.cardView
+        cardView2 = binding.cardViewEdit
+        btnScan = binding.btnScan
+        btnEntercode = binding.btnEnter
+        editCode = binding.editTextCode
+        tvText = binding.txtText
+
+        tvText!!.setText("Scan QR Code")
+        cardView2!!.visibility = View.VISIBLE
+
+        btnScan!!.setOnClickListener {
+            cardView2!!.visibility = View.VISIBLE
+            cardView1!!.visibility = View.GONE
+            tvText!!.setText("Scan QR Code")
+        }
+
+        btnEntercode!!.setOnClickListener {
+            cardView2!!.visibility = View.GONE
+            cardView1!!.visibility = View.VISIBLE
+            tvText!!.setText("Enter QR Code Here")
+        }
     }
 }
